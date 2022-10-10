5c30434c1f493a9238b23e16305b25780b2027e0
Gilbertkitetu <gilbertkitetu2@gmail.com>
2022-10-10 12:48:46 +0300

    first commit

---
+++ src/main/java/natujengegroup1com/example/springbootcrudapis_group1/SpringbootcrudapisGroup1Application.java
@@ -0,0 +1,13 @@
+package natujengegroup1com.example.springbootcrudapis_group1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcrudapisGroup1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcrudapisGroup1Application.class, args);
+	}
+
+}
---
+++ src/test/java/natujengegroup1com/example/springbootcrudapis_group1/SpringbootcrudapisGroup1ApplicationTests.java
@@ -0,0 +1,13 @@
+package natujengegroup1com.example.springbootcrudapis_group1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcrudapisGroup1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
